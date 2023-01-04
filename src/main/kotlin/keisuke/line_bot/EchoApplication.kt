50acfcc47dc70ea20c29e6a0a95e9ac5250083fc
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2023-01-04 23:48:54 +0900

    update クラス変更

---
+++ src/main/kotlin/keisuke/line_bot/EchoApplication.kt
@@ -0,0 +1,34 @@
+package keisuke.line_bot
+
+import com.linecorp.bot.model.event.Event
+import com.linecorp.bot.model.event.MessageEvent
+import com.linecorp.bot.model.event.message.TextMessageContent
+import com.linecorp.bot.model.message.TextMessage
+import com.linecorp.bot.spring.boot.annotation.EventMapping
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+@SpringBootApplication
+@LineMessageHandler
+class EchoApplication {
+	@EventMapping
+	fun handleTextMessageEvent(event: MessageEvent<TextMessageContent>): TextMessage {
+		println("event: $event")
+		return TextMessage(event.message.text)
+	}
+
+	@EventMapping
+	fun handleDefaultMessageEvent(event: Event) {
+		println("event: $event")
+	}
+
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			SpringApplication.run(EchoApplication::class.java, *args)
+		}
+	}
+}
+
