e90490d26f3cde15cdaf15bb23a4c2095567345a
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2023-01-04 23:41:44 +0900

    add 一旦諸々コミット

---
+++ src/test/kotlin/keisuke/line_bot/LineBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package keisuke.line_bot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LineBotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
