8f21e30088c5d8d614753b02c404e9056077cac5
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2023-01-13 22:35:16 +0900

    update githubを参考に変更

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,33 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.7"
-	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
+	id("org.springframework.boot") version "2.6.3"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.6.10"
+	kotlin("plugin.spring") version "1.6.10"
 }
 
-group = "keisuke"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+group = "me.r09i"
+version = "0.1.0"
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+java.targetCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("com.linecorp.bot:line-bot-spring-boot:5.0.3")
+
+	implementation("com.linecorp.bot:line-bot-api-client:4.7.0")
+	implementation("com.linecorp.bot:line-bot-spring-boot:4.7.0")
+
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
@@ -28,6 +36,11 @@ tasks.withType<KotlinCompile> {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "17"
 	}
+}
+
+tasks.getByName<Jar>("jar") {
+	// Don't let plain.jar be generated
+	enabled = false
 }
 
 tasks.withType<Test> {
--- src/main/kotlin/keisuke/line_bot/EchoApplication.kt
+++ src/main/kotlin/keisuke/line_bot/EchoApplication.kt
@@ -3,32 +3,29 @@ package keisuke.line_bot
 import com.linecorp.bot.model.event.Event
 import com.linecorp.bot.model.event.MessageEvent
 import com.linecorp.bot.model.event.message.TextMessageContent
+import com.linecorp.bot.model.message.Message
 import com.linecorp.bot.model.message.TextMessage
 import com.linecorp.bot.spring.boot.annotation.EventMapping
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-
 @SpringBootApplication
 @LineMessageHandler
 class EchoApplication {
 	@EventMapping
-	fun handleTextMessageEvent(event: MessageEvent<TextMessageContent>): TextMessage {
+	fun handleTextMessageEvent(event: MessageEvent<TextMessageContent>): Message {
 		println("event: $event")
 		return TextMessage(event.message.text)
 	}
 
-	@EventMapping
-	fun handleDefaultMessageEvent(event: Event) {
-		println("event: $event")
-	}
+//	@EventMapping
+//	fun handleDefaultMessageEvent(event: Event) {
+//		println("event: $event")
+//	}
 
-	companion object {
-		@JvmStatic
-		fun main(args: Array<String>) {
-			SpringApplication.run(EchoApplication::class.java, *args)
-		}
-	}
 }
 
+fun main(args: Array<String>) {
+	SpringApplication.run(EchoApplication::class.java, *args)
+}
